e8e3666d91cf6ec5d911b8754c2258c798cb6bc2
Keshav Sharma <keshavsharma2868@gmail.com>
2024-11-07 15:57:56 -0700

    Update While-Loops.py

--- While-Loops.py
+++ While-Loops.py
@@ -1,7 +1,45 @@
-i = 0
-while i < 10:
-    print(i)
-    if(i == 5):
-        break
-    i += 1
-    
+# i = 0
+# while i < 10:
+#     print(i)
+#     if(i == 5):
+#         break
+#     i += 1
+
+
+# n = int(input("Enter the number"))
+# sum = 0
+# i = 1
+# while i < n:
+#     sum += i
+#     i +=1
+# print(sum)   
+
+
+# n = int(input("Enter the number"))
+# i = 1
+# while i  < n:
+#     print(i)
+#     i += 1
+
+
+# chat #num = 1234
+# sum_of_digits = 0
+# while num > 0:   
+## Add the last digit to sum_of_digits
+#     sum_of_digits += num % 10   
+#     num //= 10
+# print("Sum of digits:", sum_of_digits)
+
+
+# Chat #num = 456
+# reversed_num = 0
+# while num > 0:
+#     last_digit = num % 10
+#     # Add the last digit to the reversed number in the correct place
+#     reversed_num = reversed_num * 10 + last_digit
+#     num //= 10
+
+# print("Reversed number:", reversed_num)
+
+
+
