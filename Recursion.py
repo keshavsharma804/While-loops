5563f775f9ae8163a24ef069776aa645d378369a
Keshav Sharma <keshavsharma2868@gmail.com>
2024-11-07 17:14:02 -0700

    Create Recursion.py

---
+++ Recursion.py
@@ -0,0 +1,19 @@
+# def factorial(n):
+#     
+#     if n == 0 or n == 1:
+#         return 1
+#     
+#     return n * factorial(n - 1)
+
+# n = int(input("Enter a number: "))
+# print("Factorial:", factorial(n))
+
+
+# def sum_of_natural_numbers(n):
+   
+#     if n == 0:
+#         return 0
+#     return n + sum_of_natural_numbers(n - 1)
+
+# n = int(input("Enter a number: "))
+# print("Sum of first", n, "natural numbers:", sum_of_natural_numbers(n))
